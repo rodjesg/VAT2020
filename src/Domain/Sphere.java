cf611dfe72278df0ec44d2ffa19af8db9d402624
iStyn <45605792+iStynP@users.noreply.github.com>
2020-10-19 10:24:07 +0200

    Update Sphere.java

--- src/Domain/Sphere.java
+++ src/Domain/Sphere.java
@@ -4,5 +4,44 @@ package Domain;
  * @author Rodney
  */
 public class Sphere extends ThreeDimensionalShape{
+    private double radius;
 
+    public Sphere(double radius) {
+        super("sphere");
+        this.radius = radius;
+    }
+
+    public double getDiameter() {
+        return radius * 2;
+    }
+
+    /**
+     * Calculate volume sphere
+     * V = (4/3)*π*r^3
+     * @return volume
+     */
+    @Override
+    public double getVolume() {
+        return (4.00/3.00)*Math.PI*Math.pow( this.radius,3 );
+    }
+
+    /**
+     * Calculate total surface of sphere
+     * 4*π*r^2
+     * @return totalSurface
+     */
+    @Override
+    public double getTotalSurface() {
+        return 4.00*Math.PI*(this.radius*this.radius);
+    }
+
+    @Override
+    public String getNameOfShape() {
+        return super.getNameOfShape();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
