b438fdbfa47d0d1b41e6cf28377cd140b4f445d7
iStyn <45605792+iStynP@users.noreply.github.com>
2020-10-19 10:24:59 +0200

    Update ThreeDimensionalShape.java

--- src/Domain/ThreeDimensionalShape.java
+++ src/Domain/ThreeDimensionalShape.java
@@ -1,13 +1,28 @@
 package Domain;
 
-/**
- * @author Rodney
- */
-public class ThreeDimensionalShape implements Shapes {
+public class ThreeDimensionalShape implements Shape{
+    private double volume;
+    private double totalSurface;
+    private String name;
 
+    public ThreeDimensionalShape(String name) {
+        this.name = name;
+    }
 
-    @Override
+    public double getVolume() {
+        return volume;
+    }
+
+    public double getTotalSurface() {
+        return totalSurface;
+    }
+
     public String getNameOfShape() {
-        return null;
+        return this.name;
+    }
+
+    public String toString() {
+        return "This " + name + " has a total surface of " + getTotalSurface() + " cm2\n" +
+                "and a volume of " + getVolume() + " cm3";
     }
 }
